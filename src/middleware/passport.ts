e7fadb47561f0a303736962815143cc0be057b76
anxbell <valerieanabell@gmail.com>
2025-04-01 14:33:01 -0600

    callback url corrected to 'https://cse341a2-moviedb-ts.onrender.com'

--- src/middleware/passport.ts
+++ src/middleware/passport.ts
@@ -14,7 +14,7 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
       callbackURL:
         process.env.NODE_ENV === 'production'
-          ? 'https://cse341a2-moviedb-ts.onrender.com/auth/google/callback'
+          ? 'https://cse341a2-moviedb-ts.onrender.com/auth/google/callback' //URL corrected
           : 'http://localhost:3000/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
--- src/routes/mediaTypes.ts
+++ src/routes/mediaTypes.ts
@@ -53,7 +53,11 @@ router.get('/', getAllMedia);
  *       401:
  *         description: Unauthorized access to add media type.
  */
-router.post('/', authenticateJWT, authorizeRoles('admin'), addMediaType);
+router.post(
+  '/', 
+  authenticateJWT, 
+  authorizeRoles('admin'), 
+  addMediaType);
 
 /**
  * @openapi
@@ -92,7 +96,11 @@ router.post('/', authenticateJWT, authorizeRoles('admin'), addMediaType);
  *       401:
  *         description: Unauthorized access to update media type.
  */
-router.put('/:id', authenticateJWT, authorizeRoles('admin'), updateMediaType);
+router.put(
+  '/:id', 
+  authenticateJWT, 
+  authorizeRoles('admin'), 
+  updateMediaType);
 
 /**
  * @openapi
@@ -118,122 +126,10 @@ router.put('/:id', authenticateJWT, authorizeRoles('admin'), updateMediaType);
  *       401:
  *         description: Unauthorized access to delete media type.
  */
-router.delete('/:id', authenticateJWT, authorizeRoles('admin'), deleteMediaType);
+router.delete(
+  '/:id', 
+  authenticateJWT, 
+  authorizeRoles('admin'), 
+  deleteMediaType);
 
 export default router;
-
-
-// import { Router } from 'express';
-// import { authenticateJWT } from '../middleware/authMiddleware';
-// import authorizeRoles from '../middleware/roleMiddleware';
-// import { getAllMedia, addMediaType, updateMediaType, deleteMediaType} from '../controllers/mediaTypeController';
-
-// const router = Router();
-
-// /**
-//  * @openapi
-//  * /api/mediaTypes:
-//  *   get:
-//  *     summary: Retrieve a list of available media types
-//  *     tags:
-//  *       - Media Types
-//  *     responses:
-//  *       200:
-//  *         description: A list of all available media types.
-//  *       401:
-//  *         description: Unauthorized access to media types.
-//  */
-
-// //GET MEDIA TYPES
-// router.get('/', getAllMedia);
-
-// /**
-//  * @openapi
-//  * /api/mediaTypes:
-//  *   post:
-//  *     summary: Add a new media type
-//  *     tags:
-//  *       - Media Types
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             type: object
-//  *             properties:
-//  *               name:
-//  *                 type: string
-//  *               description:
-//  *                 type: string
-//  *     responses:
-//  *       201:
-//  *         description: Media type successfully added.
-//  *       400:
-//  *         description: Bad request due to missing or incorrect data.
-//  *       401:
-//  *         description: Unauthorized access to add media type.
-//  */
-// router.post('/', addMediaType);
-
-// /**
-//  * @openapi
-//  * /api/mediaTypes/{id}:
-//  *   put:
-//  *     summary: Update an existing media type
-//  *     tags:
-//  *       - Media Types
-//  *     parameters:
-//  *       - in: path
-//  *         name: id
-//  *         required: true
-//  *         description: The ID of the media type to update
-//  *         schema:
-//  *           type: string
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             type: object
-//  *             properties:
-//  *               mediaType:
-//  *                 type: string
-//  *               description:
-//  *                 type: string
-//  *     responses:
-//  *       200:
-//  *         description: Media type successfully updated.
-//  *       400:
-//  *         description: Bad request due to missing or incorrect data.
-//  *       404:
-//  *         description: Media type not found with the provided ID.
-//  *       401:
-//  *         description: Unauthorized access to update media type.
-//  */
-// router.put('/:id', updateMediaType)
-
-// /**
-//  * @openapi
-//  * /api/mediaTypes/{id}:
-//  *   delete:
-//  *     summary: Delete an existing media type
-//  *     tags:
-//  *       - Media Types
-//  *     parameters:
-//  *       - in: path
-//  *         name: id
-//  *         required: true
-//  *         description: The ID of the media type to delete
-//  *         schema:
-//  *           type: string
-//  *     responses:
-//  *       200:
-//  *         description: Media type successfully deleted.
-//  *       404:
-//  *         description: Media type not found with the provided ID.
-//  *       401:
-//  *         description: Unauthorized access to delete media type.
-//  */
-// router.delete('/:id', deleteMediaType)
-
-// export default router;
